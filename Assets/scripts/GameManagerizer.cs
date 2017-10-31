42e1ed29a2418862a84524d68daac0109aa4046f
Unknown <bf1081@nyu.edu>
2017-10-31 12:20:10 -0400

    nyu procedural generation DS;oinxvo/zxinv;oizdf'pijc'pssdsSs'ijDS'oijS

---
+++ Assets/scripts/GameManagerizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManagerizer : MonoBehaviour {
+	public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+
+		Pathmaker.total = 0;
+		//Pathmaker.total = Random.Range()
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("space")) {
+			
+			SceneManager.LoadScene ("mainLabScene"
+			);
+		//	Instantiate (sphere, transform.position, Quaternion.identity);
+
+		}
+	
+	}
+}
